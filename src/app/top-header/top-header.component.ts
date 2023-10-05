cb642b194438073e395bb66f6faf0cc032ec7cea
mentorvikash <mentorvikash@gmail.com>
2023-10-05 23:43:06 +0530

    add diffrent type of  selector

--- src/app/top-header/top-header.component.ts
+++ src/app/top-header/top-header.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-top-header',
+  selector: 'app-top-header', // use as tag
+  // selector: '[app-top-header]', // use as attribute
+  // selector: '.app-top-header', // use as class
+  // selector: '#app-top-header', // use as id
+
   templateUrl: './top-header.component.html',
   styleUrls: ['./top-header.component.css']
 })
