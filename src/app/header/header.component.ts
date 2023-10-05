884e729ac31045ee9fd8b406361a2661a7ce90d1
mentorvikash <mentorvikash@gmail.com>
2023-10-05 21:35:28 +0530

    add top header with cli

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,8 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-header',
-    template: '<h1>E-kart</h1>'
- 
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
+    // styles: ['button{ color: red; padding: 5px}'] // here we are mixing css with ts is not good practice
+    // template: '<h1>E-kart</h1>' 
+
 })
 
 export class HeaderComponent{}
