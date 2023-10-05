5527b5021998bee7f604af2ef1328c1e7eb47b6b
mentorvikash <mentorvikash@gmail.com>
2023-10-05 22:29:02 +0530

    create component for nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { BottomMenuComponent } from './header/bottom-menu/bottom-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TopHeaderComponent
+    TopHeaderComponent,
+    TopMenuComponent,
+    BottomMenuComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/header/bottom-menu/bottom-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-menu',
+  templateUrl: './bottom-menu.component.html',
+  styleUrls: ['./bottom-menu.component.css']
+})
+export class BottomMenuComponent {
+
+}
